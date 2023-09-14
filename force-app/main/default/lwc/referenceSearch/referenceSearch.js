25b5842107aac615d1669ba8ebebd0f96e5cb3aa
First Last <ssh595959@gmail.com>
2023-09-14 13:40:10 +0900

    feat : checked 속성 리셋 시작

--- force-app/main/default/lwc/referenceSearch/referenceSearch.js
+++ force-app/main/default/lwc/referenceSearch/referenceSearch.js
@@ -1,12 +1,16 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api} from 'lwc';
 import getInit from '@salesforce/apex/ReferenceSearch.getInit';
 import { toggleMoreFilter } from './searchsh.js'; 
+import { resetValue } from './searchsh.js'; 
 
 export default class ReferenceSearch extends LightningElement {
     @api title = '기본제목';
-    moreFilter = false; 
 
     handleToggleMoreFilter() {
         toggleMoreFilter.call(this); 
+    }
+
+    btnReset(){
+        resetValue.call(this);
     }
 }
--- force-app/main/default/lwc/referenceSearch/searchsh.js
+++ force-app/main/default/lwc/referenceSearch/searchsh.js
@@ -1,3 +1,8 @@
 export function toggleMoreFilter() {
     this.moreFilter = !this.moreFilter; 
 }
+
+export function resetValue(){
+    this.moreFilter = false;
+    this.isChecked = false;
+}
