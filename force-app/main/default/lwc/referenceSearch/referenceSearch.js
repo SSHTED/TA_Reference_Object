3d4b712bea0d3848086ad5011fc01ea785548f7b
First Last <ssh595959@gmail.com>
2023-09-14 13:11:57 +0900

    feat : filter 토글

--- force-app/main/default/lwc/referenceSearch/referenceSearch.js
+++ force-app/main/default/lwc/referenceSearch/referenceSearch.js
@@ -1,6 +1,12 @@
 import { LightningElement, api } from 'lwc';
 import getInit from '@salesforce/apex/ReferenceSearch.getInit';
+import { toggleMoreFilter } from './searchsh.js'; 
 
 export default class ReferenceSearch extends LightningElement {
     @api title = '기본제목';
+    moreFilter = false; 
+
+    handleToggleMoreFilter() {
+        toggleMoreFilter.call(this); 
+    }
 }
---
+++ force-app/main/default/lwc/referenceSearch/searchsh.js
@@ -0,0 +1,3 @@
+export function toggleMoreFilter() {
+    this.moreFilter = !this.moreFilter; 
+}
