4ad8bf950c92e35a22b4ed2e195472567658549e
First Last <ssh595959@gmail.com>
2023-09-27 15:43:44 +0900

    feat :combobox ing

--- force-app/main/default/lwc/contactUsRequest/contactUsRequest.js
+++ force-app/main/default/lwc/contactUsRequest/contactUsRequest.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class ContactUsRequest extends LightningElement {}
+export default class ContactUsRequest extends LightningElement {
+    options = [
+        { label: 'Option 1', value: 'option1' },
+        { label: 'Option 2', value: 'option2' },
+        // ... other options ...
+    ];
+}
