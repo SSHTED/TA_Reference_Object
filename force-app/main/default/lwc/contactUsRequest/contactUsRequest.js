365aee20b8eeb420e453904894d33174dd42e460
First Last <ssh595959@gmail.com>
2023-10-04 16:54:56 +0900

    feat : 바로 업로드 되는거 수정해야함

--- force-app/main/default/lwc/contactUsRequest/contactUsRequest.js
+++ force-app/main/default/lwc/contactUsRequest/contactUsRequest.js
@@ -1,9 +1,10 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import getInit from '@salesforce/apex/ContactUsRequest.getInit';
 
 export default class ContactUsRequest extends LightningElement {
     error;
     categoryOptions = [];
+    fileItems = [];
     selectedCategoryOpt = '';
 
     @wire(getInit)
@@ -30,7 +31,57 @@ export default class ContactUsRequest extends LightningElement {
         this.selectedCategoryOpt = event.target.value;
         console.log("selectedCategoryOpt ::::::::::: ", this.selectedCategoryOpt)
     }
-    
+
+    // 파일 업로드
+    get acceptedFormats() {
+        return ['.png', '.xlsx', '.txt', '.pptx', '.7z', '.zip'];
+    }
+
+    getIconName(fileExtension) {
+        console.log(':::::::::::::::: getIconName start ::::::::::::::::');
+
+        const iconMap = {
+            'png': 'doctype:image',
+            'xlsx': 'doctype:excel',
+            'txt': 'doctype:txt',
+            'pptx': 'doctype:ppt',
+            '7z': 'doctype:zip',
+            'zip': 'doctype:zip'
+        };
+        return iconMap[fileExtension] || 'doctype:attachment';
+    }
+
+    handleUploadFinished(event) {
+        console.log(':::::::::::::::: handleUploadFinished start ::::::::::::::::');
+
+        const uploadedFiles = event.detail.files;
+        uploadedFiles.forEach(uploadedFile =>{
+            const fileExtension = uploadedFile.name.split('.').pop();
+
+            this.fileItems = [...this.fileItems, { 
+                name: uploadedFile.name, 
+                Id: uploadedFile.Id,
+                iconName: this.getIconName(fileExtension)
+            }];
+        });
+        console.log('uploadedFiles ::::::::::: ', uploadedFiles);
+        console.log('fileItems ::::::::::: ', JSON.stringify(this.fileItems));
+        console.log('Icon Name:', this.fileItems[0].iconName);
+    }
+
+    handleUploadRemove(event) {
+        console.log(':::::::::::::::: handleUploadRemove start ::::::::::::::::');
+        const removedFileIndex = event.target.name;
+        this.fileItems = this.fileItems.filter((file, index) => index !== parseInt(removedFileIndex, 10));
+
+    }
+
+
+    handleSaveClick(){
+
+    }
+
+
 
     // items = [
     //     {
