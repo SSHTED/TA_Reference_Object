8f47a35d2d9be549ba24f9fdc959de2cbfbec1dc
First Last <ssh595959@gmail.com>
2023-10-04 15:24:11 +0900

    feat : 카테고리 선택 완료

--- force-app/main/default/lwc/contactUsRequest/contactUsRequest.js
+++ force-app/main/default/lwc/contactUsRequest/contactUsRequest.js
@@ -1,43 +1,68 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getInit from '@salesforce/apex/ContactUsRequest.getInit';
 
 export default class ContactUsRequest extends LightningElement {
-    options = [
-        { label: 'Option 1', value: 'option1' },
-        { label: 'Option 2', value: 'option2' },
-        // ... other options ...
-    ];
+    error;
+    categoryOptions = [];
+    selectedCategoryOpt = '';
 
-    items = [
-        {
-            type: 'avatar',
-            href: 'https://www.salesforce.com',
-            label: 'Avatar Pill 1',
-            src: 'https://www.lightningdesignsystem.com/assets/images/avatar1.jpg',
-            fallbackIconName: 'standard:user',
-            variant: 'circle',
-            alternativeText: 'User avatar',
-            isLink: true,
-        },
-        {
-            type: 'avatar',
-            href: '',
-            label: 'Avatar Pill 2',
-            src: 'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg',
-            fallbackIconName: 'standard:user',
-            variant: 'circle',
-            alternativeText: 'User avatar',
-        },
-        {
-            type: 'avatar',
-            href: 'https://www.google.com',
-            label: 'Avatar Pill 3',
-            src: 'https://www.lightningdesignsystem.com/assets/images/avatar3.jpg',
-            fallbackIconName: 'standard:user',
-            variant: 'circle',
-            alternativeText: 'User avatar',
-            isLink: true,
-        },
-    ];
+    @wire(getInit)
+    wiredInit({error, data}){
+        if(data){
+            console.log(':::::::::::::::: wiredInit data start ::::::::::::::::');
 
+            this.categoryOptions = data.result.categoryListVal.map(val => {
+                return {label : val, value : val};
+            });
+            console.log('data ::::::::::: ', data);
+
+            this.error = undefined;
+        }else if(error){
+            console.error('wiredInit 에러 :', error);
+
+            this.error = error;
+            this.categoryOptions = undefined;
+        }
+    }
     
+    handleCategoryChange(event){
+        console.log(':::::::::::::::: handleCategoryChange start ::::::::::::::::');
+        this.selectedCategoryOpt = event.target.value;
+        console.log("selectedCategoryOpt ::::::::::: ", this.selectedCategoryOpt)
+    }
+    
+
+    // items = [
+    //     {
+    //         type: 'avatar',
+    //         href: 'https://www.salesforce.com',
+    //         label: 'Avatar Pill 1',
+    //         src: 'https://www.lightningdesignsystem.com/assets/images/avatar1.jpg',
+    //         fallbackIconName: 'standard:user',
+    //         variant: 'circle',
+    //         alternativeText: 'User avatar',
+    //         isLink: true,
+    //     },
+    //     {
+    //         type: 'avatar',
+    //         href: '',
+    //         label: 'Avatar Pill 2',
+    //         src: 'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg',
+    //         fallbackIconName: 'standard:user',
+    //         variant: 'circle',
+    //         alternativeText: 'User avatar',
+    //     },
+    //     {
+    //         type: 'avatar',
+    //         href: 'https://www.google.com',
+    //         label: 'Avatar Pill 3',
+    //         src: 'https://www.lightningdesignsystem.com/assets/images/avatar3.jpg',
+    //         fallbackIconName: 'standard:user',
+    //         variant: 'circle',
+    //         alternativeText: 'User avatar',
+    //         isLink: true,
+    //     },
+    // ];
+
+
 }
