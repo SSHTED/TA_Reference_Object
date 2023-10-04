a532b91ecc7310e582576cffc555ee1fafec2ac7
First Last <ssh595959@gmail.com>
2023-10-04 13:46:38 +0900

    feat : html css 완료

--- force-app/main/default/lwc/contactUsRequest/contactUsRequest.js
+++ force-app/main/default/lwc/contactUsRequest/contactUsRequest.js
@@ -6,4 +6,38 @@ export default class ContactUsRequest extends LightningElement {
         { label: 'Option 2', value: 'option2' },
         // ... other options ...
     ];
+
+    items = [
+        {
+            type: 'avatar',
+            href: 'https://www.salesforce.com',
+            label: 'Avatar Pill 1',
+            src: 'https://www.lightningdesignsystem.com/assets/images/avatar1.jpg',
+            fallbackIconName: 'standard:user',
+            variant: 'circle',
+            alternativeText: 'User avatar',
+            isLink: true,
+        },
+        {
+            type: 'avatar',
+            href: '',
+            label: 'Avatar Pill 2',
+            src: 'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg',
+            fallbackIconName: 'standard:user',
+            variant: 'circle',
+            alternativeText: 'User avatar',
+        },
+        {
+            type: 'avatar',
+            href: 'https://www.google.com',
+            label: 'Avatar Pill 3',
+            src: 'https://www.lightningdesignsystem.com/assets/images/avatar3.jpg',
+            fallbackIconName: 'standard:user',
+            variant: 'circle',
+            alternativeText: 'User avatar',
+            isLink: true,
+        },
+    ];
+
+    
 }
